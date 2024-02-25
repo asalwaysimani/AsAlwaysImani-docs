c8ae34f001883565d051f70b1a1d586538b04c65
Imani <margaretcatterdev@gmail.com>
2024-02-24 18:59:21 -0800

    please let this work

--- README.md
+++ README.md
@@ -1,37 +1,41 @@
----
-title: README
-author: Imani
-keywords:
+# Website
 
----
+This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
 
-|             Master Project              | Sub Project                           | Format                  | Status                  | Major Goal                     |
-| :-------------------------------------: | ------------------------------------- | ----------------------- | ----------------------- | ------------------------------ |
-|             High Metropolys             |                                       |                         |                         |                                |
-|                                         | The Empire of Unsheathed Knives       | TTRPG Setting/Adventure | Ideation                | Flesh out plot outline         |
-|                                         | Those Broken & Beloved Things         | TTRPG                   | Rough Draft             | Revise mechanics notes         |
-|                                         | The Red Lich Queen of Hedgeline       | TTRPG Setting/Adventure | On Hiatus               | Reread & make notes            |
-|          Neo Cities Chronicles          |                                       |                         |                         |                                |
-|                                         | Magical Girls & Mecha                 | Play By Post RPG        | In Revisions            |                                |
-|                                         | Magical Girl Mecha Rising             | Roleplay Board Game     | In Revisions            | Complete Game Design Document  |
-|                                         | Neo Cities Chronicles                 | TTRPG System            | In Revisions            | Revise & send for notes/review |
-|                                         | Key's To The Door                     | TTRPG Adventure         | On Hiatus               | Outline                        |
-|           Look Ma, We Made It           |                                       | VR Rhythm Visual Novel  |                         |                                |
-|       All The Faces of Dante Hall       |                                       | Visual Novel            |                         |                                |
-|        Magical Space Adventures         |                                       | Visual Novel            |                         |                                |
-|            Hurricane Like Me            |                                       | Visual Novel            | In Progress             | Revise & finish game script    |
-|       These Kids Will Wear Crowns       |                                       | Serialized Prose        |                         |                                |
-|             Eight Kingdoms              |                                       | Prose Novel Series      |                         |                                |
-| The Galaxy is Limitless (And So Are We) |                                       |                         |                         |                                |
-|                                         | A King For My Kingdom (Holly's Story) | Webcomic                | Plottr Outline Complete |
-|                                         | This Old Black Magic (Ashe's Story)   | Webcomic                | Plottr Outline Complete |
+### Installation
 
-![AsAlwaysImani-docs](https://socialify.git.ci/asalwaysimani/AsAlwaysImani-docs/image?description=1&descriptionEditable=The%20always%20under-construction%20website%20and%20blog%20for%20the%20various%20workings%20on%20the%20internet.%20Built%20using%20Docusaurus%20v2&font=Bitter&name=1&owner=1&pattern=Charlie%20Brown&stargazers=1&theme=Dark)
-[![Netlify Status](https://api.netlify.com/api/v1/badges/c8c9f1b0-7311-4caa-a513-b1f5fd7ae5f2/deploy-status)](https://app.netlify.com/sites/asalwaysimani-docs/deploys)
+```
+$ yarn
+```
 
-Imani can be found doing all the things all over the internet, such as game development & design projects, multidisciplinary creative writing works, live lets plays of solo tabletop board games, and then some.
+### Local Development
 
-They are in a long-term emotional relationship with Pop Punk.
+```
+$ yarn start
+```
 
-You can find them live streaming on [twitch](https://www.twitch.tv/asalwaysimani). 
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
 
+### Build
+
+```
+$ yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+Using SSH:
+
+```
+$ USE_SSH=true yarn deploy
+```
+
+Not using SSH:
+
+```
+$ GIT_USER=<Your GitHub username> yarn deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,61 +1,60 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "As Always Imani",
-  tagline: "doing all the things all over the internet",
-  url: "https://asalwaysimani.com/",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/asalwaysimani_wordmark.ico",
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "As Always Imani", // Usually your GitHub org/user name.
-  projectName: "As Always Imani Docs", // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
-  // Even if you don't use internationalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
-    path: 'i18n',
-    localeConfigs: {
-      en: {
-        label: 'English',
-        direction: 'ltr',
-        htmlLang: 'en-US',
-        calendar: 'gregory',
-        path: 'en',
-      },
-    },
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: "https://github.com/asalwaysimani/AsAlwaysImani-docs",
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-         // editUrl: "https://github.com/asalwaysimani/AsAlwaysImani-docs",
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: './src/css/custom.css',
         },
       }),
     ],
@@ -64,96 +63,79 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: "As Always Imani",
+        title: 'My Site',
         logo: {
-          alt: "As Always Imani Logo",
-          src: "img/asalwaysimani_wordmark.svg",
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Intro",
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
           },
-          { to: "/blog", label: "on CoHost", position: "left" },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: "https://cohost.org/asalwaysimani/",
-            label: "cohost",
-            position: "right",
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Intro",
-                to: "/docs/intro",
+                label: 'Tutorial',
+                to: '/docs/intro',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Links",
-                to: "/docs/contact",
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
-                label: "Twitch",
-                href: "https://www.twitch.tv/asalwaysimani",
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
               },
               {
-                label: "Ko-fi",
-                href: "https://ko-fi.com/asalwaysimani",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/asalwaysimani",
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
               },
             ],
           },
           {
-            title: "Margaret Catter Development",
+            title: 'More',
             items: [
               {
-                label: "Projects",
-                to: "src/pages/projects/MargaretCatterDevelopment",
+                label: 'Blog',
+                to: '/blog',
               },
               {
-                label: "Itch.io",
-                href: "https://margaretcatter.itch.io/",
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
     }),
 };
 
-module.exports = {
-  title: 'As Always Imani',
-  url: 'https://asalwaysimani.com', // Url to your site with no trailing slash
-  baseUrl: '/', // Base directory of your site relative to your repo
-  // ...
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        theme: {
-          customCss: [require.resolve('./src/css/custom.css')],
-        },
-      },
-    ],
-  ],
-};
+export default config;
